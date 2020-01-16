77daa19e335ffdcb3bdfb86a1db0242134b8de50
Bryan Phelps <bryphe@outrunlabs.com>
2020-01-16 15:41:11 -0500

    Remove extra logging

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -4,18 +4,8 @@ clang++ -v
 
 esy install
 
-
-esy echo '#{esy-libjpeg-turbo.lib}'
-esy ls '#{esy-libjpeg-turbo.lib}'
-
 echo "-- ESY BUILD"
 esy build
-
-esy ls '#{esy-libjpeg-turbo.lib}'
-
-echo "-- TRY LS HARDCODED PATH"
-
-ls /root/.esy/3__________________________________________________________________________/i/esy_libjpeg_turbo-a63bd461/lib
 
 echo "-- EXAMPLE INSTALL / BUILD"
 esy @example install
